c7b5fb3856133b3456ca4dfa3f630aee441c7a90
Paul Denlinger <pmdenlinger@icloud.com>
2026-04-09 14:05:57 -0700

    Add all blogs endpoint

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ app = FastAPI()
 def index():
     return { 'message' : ' Hello world!'}
 
+app.get('/blog/all')
+def get_all_blogs():
+    return { 'message' : 'All blogs provided'}
+
 @app.get('/blog/{id}')
 def get_blog(id: int):
     return { 'message' : f'Blog with {id}'}
