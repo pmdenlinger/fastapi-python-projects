a87f368c709944d333bb35cf04fc391b9f2df17a
Paul Denlinger <pmdenlinger@icloud.com>
2026-04-19 12:43:20 -0700

    Add comments for combined path and query parameters

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ def index():
 def get_blogs(page = 1, page_size = 10):
     return { 'message' : f'All {page_size} blogs on page {page}'}
 
-@app.get('/blog/{id}/{comment_id}')
+@app.get('/blog/{id}/comments/{comment_id}')
 def get_comment(id: int, comment_id: int, valid: bool = True, username: Optional [str] = None):
     return { 'message' : f'blog_id {id}, comment_id {comment_id}, valid {valid}, username {username}'}
 
